1e2d60b90cf942d5a6674c79bd2a7b5334920a35
biscon <biscon@gmail.com>
2026-05-15 23:57:59 +0200

    stuff

--- assets/cutscenes/post_beach_house/post_beach_house.lua
+++ assets/cutscenes/post_beach_house/post_beach_house.lua
@@ -1,22 +1,65 @@
 function Cutscene_onEnter()
+    showImage("slide1", 0)
     fadeFromBlack(900)
-
-    showImage("car_drive", 1000)
     delay(300)
+
     showText("I left the house the way I came in.", 500)
     delay(2200)
     clearText(400)
+    delay(200)
 
-    showText("Only this time, I knew it wasn't over.", 500)
+    showText("Only this time, I knew it wasn’t over.", 500)
     delay(2400)
     clearText(400)
+    delay(200)
 
-    showImage("chapel_road", 900)
+    showText("You don’t send that many men unless you’re trying to bury something.", 500)
+    delay(3200)
+    clearText(400)
     delay(300)
-    showText("And then there was the other thought.", 500)
+
+    showImage("slide2", 900)
+    delay(300)
+
+    showText("The woman on the phone knew where to find me.", 500)
+    delay(2400)
+    clearText(400)
+    delay(200)
+
+    showText("That meant she was connected.", 500)
     delay(2200)
     clearText(400)
+    delay(200)
+
+    showText("To them...", 500)
+    delay(1500)
+    clearText(400)
+    delay(200)
+
+    showText("or to what I took from them.", 500)
+    delay(2400)
+    clearText(400)
+    delay(300)
+
+    showImage("slide3", 900)
+    delay(300)
+
+    showText("And then there was the other thought.", 500)
+    delay(2400)
+    clearText(400)
+    delay(200)
+
+    showText("The one I’d spent years trying not to think...", 500)
+    delay(2800)
+    clearText(400)
+    delay(200)
+
+    showText("could she still be alive after all this time?", 500)
+    delay(2800)
+    clearText(400)
+    delay(300)
 
     fadeToBlack(900)
+    delay(900)
     endCutscene()
 end
