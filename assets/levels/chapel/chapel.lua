a77b33f5504ccdb1b8c85544d55dc9abdc2b3b99
biscon <biscon@gmail.com>
2026-05-22 20:46:52 +0200

    chapel

--- assets/levels/chapel/chapel.lua
+++ assets/levels/chapel/chapel.lua
@@ -3,6 +3,11 @@ local Glow = require("effects.glow")
 function Level_onEnter()
     log("on enter")
     startScript("StreetLampGlowLoop")
+    startScript("ShedLampGlowLoop")
+    startScript("ChapelLamp1GlowLoop")
+    startScript("ChapelLamp2GlowLoop")
+    startScript("ChapelLamp3GlowLoop")
+    startScript("ChapelLamp4GlowLoop")
 
     addEquipmentSet("handgun")
     equipEquipmentSet("handgun")
@@ -18,5 +23,45 @@ function StreetLampGlowLoop()
         { "unused", "street_lamp_glow" },
         0.00,
         0.45
+    )
+end
+
+function ShedLampGlowLoop()
+    Glow.runElectric(
+        { "unused", "shed_lamp_glow" },
+        0.00,
+        0.25
+    )
+end
+
+function ChapelLamp1GlowLoop()
+    Glow.runFire(
+        { "unused", "chapel_lamp1_glow" },
+        0.00,
+        0.25
+    )
+end
+
+function ChapelLamp2GlowLoop()
+    Glow.runFire(
+        { "unused", "chapel_lamp2_glow" },
+        0.00,
+        0.25
+    )
+end
+
+function ChapelLamp3GlowLoop()
+    Glow.runFire(
+        { "unused", "chapel_lamp3_glow" },
+        0.00,
+        0.25
+    )
+end
+
+function ChapelLamp4GlowLoop()
+    Glow.runFire(
+        { "unused", "chapel_lamp4_glow" },
+        0.00,
+        0.25
     )
 end
